6fe18bba2b15bc1de892d6b308178a134a16e9b6
SantiagoDiFede <100380597+SantiagoDiFede@users.noreply.github.com>
2023-03-10 10:04:14 +0100

    Add files via upload

--- R410/TP2/README.md
+++ R410/TP2/README.md
@@ -1,1 +1,10 @@
 # M413 - TD2 : Réponses aux Questions
+
+6.1
+
+    1.On a ajouté un event listener sur l'objet avec l'ID titre.
+
+    2.currentTarget est l'élément auquel l'événement est attaché et target est l'élément qui 
+    reçoit l'événement
+
+    
--- R410/TP2/js/td2.js
+++ R410/TP2/js/td2.js
@@ -17,6 +17,8 @@ console.log( message);
 
 function onLoad() {
 	console.log( 'Processus de chargement du document terminé…');
+	initSelect();
+	loadDiv();
 	//
 	// All your JavaScript code goes here !
 	//
@@ -26,17 +28,91 @@ function onLoad() {
 // Toute les ressources de la page sont complètement chargées.
 window.onload = onLoad;
 
-
 function initSelect() {
-	//change the color of h1 when clicked
-	document.getElementById("title").addEventListener("click", function(){
-		this.style.color = "red";
-	});
+
+	document.addEventListener("click", select);
+	document.addEventListener("click", select2);
+
 
 
 }
-for (let i = 0; i < 4; i++) {
-	document.getElementsByTagName("h1").item(i).addEventListener("click", function(){
-		this.style.color = "red";
-	});
+
+
+function select(event) {
+
+	let target = event.target;
+	if(target === undefined){
+		return;
+	}
+	else if(target.style.backgroundColor === "red"){
+		target.style.backgroundColor = "white";
+	}
+	else {
+		target.style.backgroundColor = "red";
+	}
+}
+
+
+function loadDiv(){
+	let div = document.createElement("div");
+
+	div.setAttribute("id", "insert-div");
+
+	let select = document.createElement("select");
+
+	select.setAttribute("id", "insert-select");
+
+	select.setAttribute("name", "type");
+
+	let option1 = document.createElement("option");
+	option1.setAttribute("value", "div");
+	option1.innerHTML = "div";
+	let option2 = document.createElement("option");
+	option2.setAttribute("value", "p");
+	option2.innerHTML = "p";
+	let option3 = document.createElement("option");
+	option3.setAttribute("value", "span");
+	option3.innerHTML = "span";
+
+	select.appendChild(option1);
+	select.appendChild(option2);
+	select.appendChild(option3);
+	div.appendChild(select);
+	let input = document.createElement("input");
+	input.setAttribute("type", "text");
+	input.setAttribute("id", "insert-text");
+	input.setAttribute("value", "My New Text Element");
+	div.appendChild(input);
+
+
+	document.body.insertBefore(div, document.body.firstChild);
+}
+
+function select2(event){
+	let target = event.target;
+	if(target == undefined){
+		return;
+	}
+	target.style.backgroundColor = "blue";
+
+
+	//make the background of all other elements not blue except the one clicked
+	let allElements = document.getElementsByTagName("*");
+	for(let i = 0; i < allElements.length; i++){
+		if(allElements[i] == undefined){
+			continue;
+		}
+		if(allElements[i] != target){
+			allElements[i].style.backgroundColor = "white";
+		}
+
+	}
+}
+
+function insertElement(target){
+	let select = document.getElementById("insert-select");
+	let text = document.getElementById("insert-text");
+	let element = document.createElement(select.value);
+	element.innerHTML = text.value;
+	target.parentNode.insertBefore(element, target);
 }
